35d2d56685391b803f91f1ed37c4ef8d729072bb
SLGBOT <47791078+SLGBOT@users.noreply.github.com>
2019-03-03 10:32:39 +0100

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -108,7 +108,7 @@ Client.on("message", async (message) => {
             .setColor('#C5370A')
             .setTitle("__SLG | Soutiens__")
             .setDescription("Vous avez effectué la commande soutiens.")
-            .addField("__Nombre de soutiens__", "``26 Soutiens | Approximativement $18``")
+            .addField("__Nombre de soutiens__", "``30 Soutiens | Approximativement $22.00``")
             .setFooter("Soutien nous : SUPERLEAGUEGAME")
         message.channel.sendEmbed(help_embed);
         //messagechannel
